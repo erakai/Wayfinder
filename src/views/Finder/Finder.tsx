f8d6e048f80e9aa0a5e1acde137ef35485bd3d5d
AlexHunton2 <alexhunton02@gmail.com>
2023-01-21 14:00:12 -0500

    Finder Initial Commit

---
+++ src/views/Finder/Finder.tsx
@@ -0,0 +1,11 @@
+import TopBar from "../../components/Structure/TopBar"
+import DestinationGrid from "../../components/Structure/DestinationGrid"
+
+export default function Finder() {
+  return (
+    <div>
+      <TopBar />
+      <DestinationGrid />
+    </div>
+  )
+}
--- src/views/Root/Root.tsx
+++ src/views/Root/Root.tsx
@@ -1,15 +1,15 @@
-import { Container } from '@mui/material'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Editor from '../Editor'
+import Finder from "../Finder"
 
 export default function Root() {
     return (
       <BrowserRouter>
         <Routes>
-          <Route index element={<Editor />} />
-
-          <Route path="*" element={<Container sx={{ p: 5 }}>404</Container>} />
+          <Route index path="/" element={<Finder />} />
+          <Route index path="/editor" element={<Editor />} />
+          <Route path="*" element={<h1>Error: 404</h1>} />
         </Routes>
       </BrowserRouter>
     )
